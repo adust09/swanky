b8be386e40bd3bb13fac5a3d35d08ff4cd4eafdb
adust09 <shoukitsuda@icloud.com>
2025-04-16 00:47:48 +0900

    Remove unused imports in prover_preparer.rs and add unit test for f64b_to_ark conversion in field_mappings.rs

--- schmivitz-snark/src/field_mappings.rs
+++ schmivitz-snark/src/field_mappings.rs
@@ -53,3 +53,17 @@ pub fn f128b_to_ark(value: &F128b) -> Bn254Fr {
     // Convert the u64 value to a Bn254Fr field element
     Bn254Fr::from(u64_value)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fr as Bn254Fr;
+    use swanky_field_binary::F64b;
+
+    #[test]
+    fn test_f64b_to_ark() {
+        let f64b = F64b::from(123456789u64);
+        let ark = f64b_to_ark(&f64b);
+        assert_eq!(ark, Bn254Fr::from(123456789u64));
+    }
+}
